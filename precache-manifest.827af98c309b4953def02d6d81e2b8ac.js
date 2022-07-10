3d822ae43cd68955f2af11939de8813f0a8c82bb
Alexandra Garcia <45516972+alexg1299@users.noreply.github.com>
2022-07-09 19:43:19 -0500

    Updates

---
+++ precache-manifest.827af98c309b4953def02d6d81e2b8ac.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e92cffac36c0313d5ba92e603b414e1d",
+    "url": "/alexg1299/portfolio-react/index.html"
+  },
+  {
+    "revision": "bc5094eb2b259146b929",
+    "url": "/alexg1299/portfolio-react/static/css/2.a4c8f85b.chunk.css"
+  },
+  {
+    "revision": "644fa6d912858ec7d47e",
+    "url": "/alexg1299/portfolio-react/static/css/main.066f48e7.chunk.css"
+  },
+  {
+    "revision": "bc5094eb2b259146b929",
+    "url": "/alexg1299/portfolio-react/static/js/2.8da2113e.chunk.js"
+  },
+  {
+    "revision": "02af6404522a60bdc42641cd9a090cbe",
+    "url": "/alexg1299/portfolio-react/static/js/2.8da2113e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "644fa6d912858ec7d47e",
+    "url": "/alexg1299/portfolio-react/static/js/main.d1fcc099.chunk.js"
+  },
+  {
+    "revision": "0cd7fedefef1b16547a7",
+    "url": "/alexg1299/portfolio-react/static/js/runtime-main.fcc9c2ec.js"
+  },
+  {
+    "revision": "67af05e12311bac06327b43ca31e166b",
+    "url": "/alexg1299/portfolio-react/static/media/earth.67af05e1.jpg"
+  },
+  {
+    "revision": "b61f96d47d0a29b0e7e18a273cbea286",
+    "url": "/alexg1299/portfolio-react/static/media/sample.b61f96d4.png"
+  }
+]);
